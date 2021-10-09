d368dfed5816545a5681db5b6daca76e1e2aea89
Jonics7 <jonics7@mail.ru>
2021-10-09 15:49:11 +0300

    made dropdowns

--- packages/frontend/src/Components/Dropdown/Dropdown.tsx
+++ packages/frontend/src/Components/Dropdown/Dropdown.tsx
@@ -2,22 +2,40 @@ import React, { useState } from 'react';
 import './Dropdown.scss';
 import { ReactComponent as Angle } from '../../Assets/Icons/angle.svg';
 
-const DropdownItems = ['One To One', 'Many To Many'] as const;
-type DropdownItemsType = typeof DropdownItems[number];
+interface DropdownProps<T extends string> {
+    items: T[] | readonly T[];
+    onItemChange: (value: T) => void;
+}
 
-const Dropdown: React.FC = () => {
-    const [selected, setSelected] = useState<DropdownItemsType>('One To One');
+function Dropdown<T extends string>({ items, onItemChange }: DropdownProps<T>) {
+    const [selected, setSelected] = useState<T>(items[0]);
     const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+    const onItemClick = (text: T) => {
+        setSelected(text);
+        setIsExpanded(false);
+    };
 
     return (
         <div className="Dropdown">
-            <div className="Dropdown-selected">
+            <div className="Dropdown-selected" onClick={() => setIsExpanded(!isExpanded)}>
                 <div className="Dropdown-selected-text">{selected}</div>
                 <Angle className="Dropdown-arrow" />
             </div>
-            <div className={`Dropdown-items${isExpanded ? 'expanded' : ''}`}></div>
+            <div className={`Dropdown-items${isExpanded ? ' expanded' : ''}`}>
+                {items
+                    .filter((item: T) => item !== selected)
+                    .map((item, index) => (
+                        <div className="Dropdown-item" key={index} onClick={() => onItemClick(item)}>
+                            {item}
+                            {items.findIndex((value) => value === item) !== items.length - 1 ? (
+                                <div className="Dropdown-line" />
+                            ) : null}
+                        </div>
+                    ))}
+            </div>
         </div>
     );
-};
+}
 
 export default Dropdown;
--- packages/frontend/src/Pages/MergingPage/MergingPage.tsx
+++ packages/frontend/src/Pages/MergingPage/MergingPage.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 import './MergingPage.scss';
 import { ReactComponent as Arrow } from '../../Assets/Icons/bottom-arrow.svg';
+import { ReactComponent as Plus } from '../../Assets/Icons/doc-plus.svg';
+import { ReactComponent as Minus } from '../../Assets/Icons/doc-minus.svg';
 import Dropdown from '../../Components/Dropdown/Dropdown';
+import Helper from '../../Components/Helper/Helper';
+
+const DropdownItems = ['One To One', 'Many To Many'] as const;
+export type DropdownItemsType = typeof DropdownItems[number];
+
+const SortByItems = ['Sort By', 'Name', 'Country', 'Price'] as const;
+export type SortByItemsType = typeof SortByItems[number];
 
 const MergingPage: React.FC = () => {
     const [fields, setFields] = useState<Array<string>>(['Телефон', 'Телефон']);
@@ -9,13 +18,25 @@ const MergingPage: React.FC = () => {
 
     return (
         <div className="MergingPage">
+            <div className="MergingPage-header">
+                <div className="MergingPage-header-controls">
+                    <div className="MergingPage-header-button">
+                        <Plus />
+                    </div>
+                    <div className="MergingPage-header-button">
+                        <Minus />
+                    </div>
+                    <Dropdown items={SortByItems} onItemChange={(i) => {}} />
+                </div>
+                <Helper isExpanded handleHelper={() => {}} />
+            </div>
             <div className="MergingPage-layout">
                 <div className="MergingPage-datasets"></div>
                 <div className="MergingPage-controls">
                     <div className="MergingPage-controls-field">
                         <div className="MergingPage-controls-field-text">{fields[0]}</div>
                     </div>
-                    <Dropdown />
+                    <Dropdown items={DropdownItems} onItemChange={() => {}} />
                     <div className="MergingPage-controls-field">
                         <div className="MergingPage-controls-field-text">{fields[1]}</div>
                     </div>
