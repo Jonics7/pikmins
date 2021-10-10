38f33d8caeb5696779a70afa27aad6cc2183503d
Jonics7 <jonics7@mail.ru>
2021-10-10 07:57:06 +0300

    update visuals

--- packages/frontend/src/Components/DatasetField/DatasetField.tsx
+++ packages/frontend/src/Components/DatasetField/DatasetField.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './DatasetField.scss';
 import { ReactComponent as Link } from '../../Assets/Icons/link.svg';
+import { ReactComponent as Delete } from '../../Assets/Icons/delete.svg';
 
 interface DatasetFieldProps {
     selected: boolean;
@@ -9,14 +10,24 @@ interface DatasetFieldProps {
     onClick: () => void;
     inactive?: boolean;
     linked?: string;
+    canBeDeleted?: boolean;
 }
 
-const DatasetField: React.FC<DatasetFieldProps> = ({ selected, name, value, onClick, inactive, linked }) => {
+const DatasetField: React.FC<DatasetFieldProps> = ({
+    selected,
+    name,
+    value,
+    onClick,
+    inactive,
+    linked,
+    canBeDeleted,
+}) => {
     return (
         <div className={`DatasetField${selected ? ' selected' : ''}${inactive ? ' inactive' : ''}`} onClick={onClick}>
             <div className="DatasetField-name">{name}</div>
             <div className="DatasetField-value">{value}</div>
             {linked ? <Link className="DatasetField-link" /> : null}
+            {canBeDeleted ? <Delete className="DatasetField-delete" /> : null}
         </div>
     );
 };
